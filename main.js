154697a062a4076a542b0b29ef11c8b92db72e5d
Alex Kehayias <alex.kehayias@gmail.com>
2016-12-29 12:41:38 -0800

    Update lambda handler to add optional git commit

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ exports.handler = function(event, context, callback) {
     console.log(event);
 
     var git_url = event['body-json']['git-url'];
+    var git_commit = event['body-json']['git-commit'];
+
+    // Git commit is optional
+    if (!!git_commit) {
+	var args = [git_url, git_commit];
+    } else {
+	var args = [git_url];
+    };
 
     // spawn a child process to run the binary
     var proc = child_process.spawn(
 	'./target/release/harbor',
-	[git_url],
+	args,
 	// proc.stdin, pro.stdout, proc.stderr
 	{stdio: ['ignore', 'pipe', 'ignore']}
     );
